0a36199447a5900df619c9c2963e38dc8a2567d3
willhurley004 <willhurley004@gmail.com>
2015-02-15 12:40:51 -0500

    openshift compat

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,5 @@
 require 'securerandom'
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
@@ -20,6 +21,12 @@ def find_secure_token
     token
   end
 end
+
+
+RailsApp::Application.config.secret_token = initialize_secret(
+:token,
+'335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
+)
 
 Kandan::Application.config.secret_token  = find_secure_token
 Kandan::Application.config.secret_key_base = find_secure_token
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,10 @@
 # Be sure to restart your server when you modify this file.
-
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Kandan::Application.config.session_store :cookie_store, key: '_kandan_session'
-
+RailsApp::Application.config.session_store :cookie_store, :key => initialize_secret(
+:session_store,
+'_railsapp_session'
+)
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
